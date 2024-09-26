36abf5b3b58e744d0ba245dbb7beb612acda9e69
Dalton Claybrook <daltonclaybrook@gmail.com>
2024-09-25 21:03:26 -0500

    Update Package.swift # Conflicts: #	Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/madmachineio/SwiftIO.git", .branch("main")),
+        .package(url: "https://github.com/madmachineio/SwiftIO.git", revision: "684e2b0f7a2f61e1ccdf3bba23ee103bcf29d499"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
